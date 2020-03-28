bae29e717851cd12c4f0c6d77dc803f8c3210dcd
Christian Reichel <chrstn.rchl@gmail.com>
2020-03-28 11:20:33 +0100

    Init repository. Add script. Add rules

---
+++ README.md
@@ -0,0 +1,15 @@
+# Chinese Ad Host Little Snitch rules
+
+This repository utilizes the vokins-vhost host list of Chinese ad servers which are missing in the famous lists of Steve Black.
+
+This repository is gratefully using the resources of these open source heroes:
+- [yHosts of vokins](https://github.com/vokins/yhosts/)
+- [Little Snitch rule groups of Steve Black's hosts file (I gratefully used the converter script here)](https://github.com/naveednajam/Little-Snitch---Rule-Groups)
+- 
+
+## Subscribe to the Little Snitch rules
+Please read here: https://help.obdev.at/littlesnitch/lsc-rule-group-subscriptions
+
+## Generate new host files
+1. Install dependencies by running `pip install -r requirements.txt`
+2. Run `python converter.py` (this downloads as well the latest version of the hosts file
---
+++ converter.py
@@ -0,0 +1,73 @@
+"""
+Little snitch rules to block adware and malware websites.
+"""
+
+# Standard library imports
+import requests
+import io
+import re
+import json
+
+#----------------------------------------------------------------------
+
+class GenerateLittleSnitchRules():
+
+	HOST_FILE_URL 	= "https://raw.githubusercontent.com/vokins/yhosts/master/hosts"
+	OUT_FILENAME 	= "chinese_adware_malware_list.lsrules"
+
+	def __init__(self):
+		self.data 		= self.fetch_data()
+		self.rules		= self.extract_hosts(self.data)
+		self.generate_ruleset(self.rules)
+
+	def fetch_data(self):
+		#----------------------------------------------------------------------
+		# Retrieve host lists
+		# @return: list containing the lines
+		#----------------------------------------------------------------------
+
+		u = requests.get(self.HOST_FILE_URL)
+		return u.text
+
+	def extract_hosts(self, data):
+		#----------------------------------------------------------------------
+		# Extract urls found in sitemap and follow to submaps e.g. SEO Yoast
+		# @param: data - blacklisted host list
+		# @return: array of ls formatted domain rules
+		#----------------------------------------------------------------------
+
+		# Only keep hostnames that follow 0.0.0.0, while removing trailing comments.
+		pattern = re.compile("^0.0.0.0 ([^#]*)(#.*)?")
+
+		rules = []
+
+		for line in self.data:
+			match = pattern.search(line)
+			if match is not None:
+				# First group is the hostname, second group is the comment (optional).
+				hostname = match.group(1).strip()
+				if hostname != "0.0.0.0":
+					rules.append({"action":"deny", "process":"any", "remote-domains":hostname})
+
+		return rules
+
+	def generate_ruleset(self, rules):
+		#----------------------------------------------------------------------
+		# Generate JSON file for little snitch
+		# @param: rules - array of ls formatted domain rules
+		#----------------------------------------------------------------------
+
+		data = {}
+		data["name"] = "Block adware and malware"
+		data["description"] = "Little snitch rules to block adware and malware websites. Host lists from Steven Black."
+		data["rules"] = self.rules
+
+		# Write JSON to file and download
+		with open(self.OUT_FILENAME, 'w') as json_file:
+			json.dump(data, json_file, indent=2)
+
+# End GenerateLittleSnitchRules Class
+#----------------------------------------------------------------------
+
+if __name__ == '__main__':
+	GenerateLittleSnitchRules()
